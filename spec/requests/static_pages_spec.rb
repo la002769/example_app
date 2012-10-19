d2ce8b3532f1a32bde1a8bc443bcafcc1ce83f00
la002769 <la002769@etu.unice.fr>
2012-10-19 03:30:14 -0700

    Test avancé sur home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
+
 describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'Example App'" do
+
+    it "should have the h1 'Example App'" do
       visit '/static_pages/home'
-      page.should have_content('Example App')
+      page.should have_selector('h1', :text => 'Example App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+          :text => "Ruby on Rails Tutorial Example App | Home")
     end
   end
 	describe "Help page" do
